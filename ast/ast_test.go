cb6b2a37c4b27437d2438c07dd428182e2821ca0
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-05 22:36:21 +0800

    Added tests for AST

---
+++ ast/ast_test.go
@@ -0,0 +1,38 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/cartoon-raccoon/monkey-jit/lexer"
+)
+
+func TestString(t *testing.T) {
+	prog := Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: lexer.Token{
+					Type:    lexer.LET,
+					Literal: lexer.LET,
+				},
+				Name: &Identifier{
+					Token: lexer.Token{
+						Type:    lexer.IDENT,
+						Literal: "my_var",
+					},
+					Value: "my_var",
+				},
+				Value: &Identifier{
+					Token: lexer.Token{
+						Type:    lexer.IDENT,
+						Literal: "another_var",
+					},
+					Value: "another_var",
+				},
+			},
+		},
+	}
+	progstring := prog.String()
+	if progstring != "let my_var = another_var;" {
+		t.Fatalf("Incorrect p.String(): got %q", progstring)
+	}
+}
