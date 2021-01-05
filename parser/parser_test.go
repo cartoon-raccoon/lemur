21f4e4f1bc2143258301c0a533628c030b72c11d
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-06 00:29:20 +0800

    Added tests for integer expression statements

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -10,7 +10,7 @@ import (
 func TestLetStatements(t *testing.T) {
 	input := `let x = 10;
 	let y = 5_000;
-	let nice = 420.69;`
+	let nice = 69;`
 
 	l := lexer.New(input)
 	p, err := New(l)
@@ -44,10 +44,14 @@ func testLetStatement(t *testing.T, stmt ast.Statement, ident string) bool {
 	}
 	letstmt, ok := stmt.(*ast.LetStatement)
 	if !ok {
-		t.Errorf("statement not let statement: got %T", stmt)
+		t.Fatalf("statement not let statement: got %T", stmt)
 	}
 	if letstmt.Name.Value != ident {
 		t.Errorf("wrong identifier: got %q", letstmt.Name.Value)
+	}
+	_, ok = letstmt.Value.(*ast.Int)
+	if !ok {
+		t.Errorf("value is not int")
 	}
 
 	return true
@@ -84,9 +88,9 @@ func TestReturnStatement(t *testing.T) {
 }
 
 func TestIdentExpr(t *testing.T) {
-	testcode := "foobar;"
+	input := "foobar;"
 
-	l := lexer.New(testcode)
+	l := lexer.New(input)
 	p, err := New(l)
 	if err != nil {
 		t.Fatalf("Error: lexer encountered an error - %s", err)
@@ -102,6 +106,44 @@ func TestIdentExpr(t *testing.T) {
 	}
 
 	if stmt, ok := prog.Statements[0].(*ast.ExprStatement); !ok {
-		t.Fatalf("Statement is not expression statment, got %q", stmt.String())
+		t.Fatalf("Statement is not expression statement, got %q", stmt.String())
+		if ident, ok := stmt.Expression.(*ast.Identifier); ok {
+			if ident.Token.Type != lexer.IDENT || ident.Value != "foobar" {
+				t.Fatalf("Incorrect identifier, got %s", ident.Value)
+			}
+		} else {
+			t.Fatalf("Statement is not identifier")
+		}
+	}
+}
+
+func TestIntExpr(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p, err := New(l)
+	if err != nil {
+		t.Fatalf("Error: lexer encountered an error - %s", err)
+	}
+
+	prog, err := p.Parse()
+	if err != nil {
+		t.Fatalf("Error: parser encountered an error - %s", err)
+	}
+
+	if len := len(prog.Statements); len != 1 {
+		t.Fatalf("Not enough statements, got: %d", len)
+	}
+
+	if stmt, ok := prog.Statements[0].(*ast.ExprStatement); ok {
+		if intlit, ok := stmt.Expression.(*ast.Int); ok {
+			if intlit.Inner != 5 {
+				t.Fatalf("Integer is not 5, got %d", intlit.Inner)
+			}
+		} else {
+			t.Fatalf("Expression is not an integer")
+		}
+	} else {
+		t.Fatalf("statement is not an expression statment")
 	}
 }
