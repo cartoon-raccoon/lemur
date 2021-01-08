b0fc4415f42c446abc0764900792e191a1bfee9b
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-08 20:19:24 +0800

    Added function declarations as a node

--- ast/ast.go
+++ ast/ast.go
@@ -2,7 +2,20 @@ package ast
 
 import (
 	"bytes"
+	"strings"
+
+	"github.com/cartoon-raccoon/monkey-jit/lexer"
 )
+
+/*
+Node, Statement and Expression are generic interfaces that
+define the grammar of the language. Every language construct
+can be defined as either a statement or expression, and the
+rest are defined as literals that are stored as separate data.
+
+Node defines a single node on the AST. Both Statement and Expression
+implement Node, so that they can be represented on the AST.
+*/
 
 // Node defines the general behaviour for a node in the AST
 type Node interface {
@@ -22,9 +35,16 @@ type Expression interface {
 	expressionNode()
 }
 
+// Declaration defines a top level declaration (e.g. class, fn)
+type Declaration interface {
+	Node
+	declarationNode()
+}
+
 // Program represents the entire parsed program
 type Program struct {
 	Statements []Statement
+	Functions  []FunctionDecl
 }
 
 // TokenLiteral implements Node for string
@@ -42,5 +62,38 @@ func (p *Program) String() string {
 		out.WriteString(s.String())
 	}
 	return out.String()
+}
 
+// FunctionDecl represents a function declaration
+type FunctionDecl struct {
+	Token  lexer.Token
+	Name   string
+	Params []*Identifier
+	Body   *BlockStatement
+}
+
+func (fd *FunctionDecl) declarationNode() {}
+
+// TokenLiteral implements Node for FunctionDecl
+func (fd *FunctionDecl) TokenLiteral() string {
+	return fd.Token.Literal
+}
+
+// String implements Node for FunctionDecl
+func (fd *FunctionDecl) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range fd.Params {
+		params = append(params, p.String())
+	}
+	out.WriteString("fn ")
+	out.WriteString(fd.Name)
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fd.Body.String())
+
+	return out.String()
 }
