d2a95c25a41c709d631c616d4d480c8694f24235
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-09 23:11:08 +0800

    Added (failing) tests for eval
    The tests themselves fail, but running the repl works fine.

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -1,1 +1,63 @@
 package eval
+
+import (
+	"testing"
+
+	"github.com/cartoon-raccoon/monkey-jit/lexer"
+	"github.com/cartoon-raccoon/monkey-jit/object"
+	"github.com/cartoon-raccoon/monkey-jit/parser"
+)
+
+func TestExprStmtEval(t *testing.T) {
+	tests := []struct {
+		Input    string
+		Expected object.Object
+		Inspect  string
+	}{
+		{
+			"1 + 1",
+			&object.Integer{Value: 2},
+			"2",
+		},
+		{
+			"hello",
+			&object.String{Value: "hello"},
+			"hello",
+		},
+		{
+			"420.69 + 7.4",
+			&object.Float{Value: 428.09},
+			"428.09",
+		},
+	}
+
+	for i, test := range tests {
+		l := lexer.New(test.Input)
+		p, err := parser.New(l)
+		if err != nil {
+			t.Errorf("Test %d: Error while beginning lexing", i)
+			continue
+		}
+		prog := p.Parse()
+		if p.CheckErrors() != nil {
+			t.Errorf("Test %d: Errors while parsing", i)
+			for _, err := range p.CheckErrors() {
+				t.Logf(err.Error())
+			}
+			t.Log("Aborting tests")
+			t.FailNow()
+		}
+		if prog == nil {
+			t.Fatalf("Test %d: Program is nil", i)
+		}
+		eval := Evaluator{}
+		res := eval.Evaluate(prog)
+		if res == nil {
+			t.Fatalf("Test %d: Could not evaluate", i)
+		}
+		if res.Inspect() != test.Inspect {
+			t.Errorf("Test %d: Expected %s, got %s", i, test.Inspect, res.Inspect())
+		}
+	}
+
+}
