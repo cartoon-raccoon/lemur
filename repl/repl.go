c12034cabf7561f3583a7047e68f520e98f0325f
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-09 16:08:57 +0800

    Added parsing and evaluation to repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 	"runtime"
 
+	"github.com/cartoon-raccoon/monkey-jit/eval"
 	"github.com/cartoon-raccoon/monkey-jit/lexer"
+	"github.com/cartoon-raccoon/monkey-jit/parser"
 )
 
 // PROMPT - the prompt that the user sees
@@ -28,8 +31,8 @@ func (r *Repl) Run(username string, in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	fmt.Printf("Monkey Interactive Shell v0.1\n")
-	fmt.Printf("running on %s %s\n", runtime.GOOS, runtime.GOARCH)
-	fmt.Printf("Welcome to Monkey, %s\n", username)
+	fmt.Printf("running on (%s %s)\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("Welcome, %s\n", username)
 
 	for {
 		fmt.Printf(PROMPT)
@@ -40,14 +43,38 @@ func (r *Repl) Run(username string, in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok, err := l.NextToken(); tok.Type != lexer.EOF; tok, err = l.NextToken() {
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-			fmt.Printf("%+v\n", tok)
+		// for tok, err := l.NextToken(); tok.Type != lexer.EOF; tok, err = l.NextToken() {
+		// 	if err != nil {
+		// 		fmt.Println(err)
+		// 		break
+		// 	}
+		// 	fmt.Printf("%+v\n", tok)
+		// }
+		p, err := parser.New(l)
+		if err != nil {
+			fmt.Println("Errors encountered 1")
+			fmt.Fprintf(os.Stdout, err.Error())
+			continue
 		}
+		prog := p.Parse()
+		if p.CheckErrors() != nil {
+			fmt.Println("Errors encountered 2")
+			for _, err := range p.CheckErrors() {
+				fmt.Fprintf(os.Stdout, err.Error())
+			}
+			continue
+		}
+
+		evaluator := eval.Evaluator{}
+
+		obj := evaluator.Evaluate(prog)
+
+		if obj == nil {
+			fmt.Println("<nil>")
+			continue
+		}
+		obj.Display()
+
 	}
 
 }
