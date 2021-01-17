9a4839e4f2bf3bd402383bd299b519a564fc8392
cartoon-raccoon <shaughn.chan@gmail.com>
2021-01-17 20:43:45 +0800

    Added rudimentary VM execution

---
+++ vm/vm.go
@@ -0,0 +1,71 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/cartoon-raccoon/lemur/code"
+	"github.com/cartoon-raccoon/lemur/compiler"
+	"github.com/cartoon-raccoon/lemur/object"
+)
+
+// StackSize is the maximum size the stack can take
+const StackSize = 2048
+
+// VM represents the virtual machine
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int // stack pointer. Top of stack is stack[sp - 1]
+
+	ip int //instruction pointer
+}
+
+// New returns a new VM
+func New() *VM {
+	return &VM{
+		stack: make([]object.Object, StackSize),
+		sp:    0,
+	}
+}
+
+// StackTop returns the item at the top of the stack
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+
+	return vm.stack[vm.sp-1]
+}
+
+// Run executes the code that is given to it via the VM
+func (vm *VM) Run(bc *compiler.Bytecode) error {
+	vm.instructions = bc.Instructions
+	vm.constants = bc.Constants
+	for vm.ip = 0; vm.ip < len(vm.instructions); vm.ip++ {
+		op := code.Opcode(vm.instructions[vm.ip])
+
+		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[vm.ip+1:])
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
+			vm.ip += 2
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
---
+++ vm/vm_test.go
@@ -0,0 +1,84 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/cartoon-raccoon/lemur/compiler"
+	"github.com/cartoon-raccoon/lemur/lexer"
+	"github.com/cartoon-raccoon/lemur/object"
+	"github.com/cartoon-raccoon/lemur/parser"
+)
+
+func testIntegerObject(constant int64, obj object.Object) error {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("Expected integer, got %T", obj)
+	}
+
+	if result.Value != constant {
+		return fmt.Errorf("Values do not equate: got %d, expected %d",
+			result.Value, constant)
+	}
+
+	return nil
+}
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, //fixme
+	}
+
+	runVMTests(t, tests)
+}
+
+func runVMTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p, err := parser.New(l)
+		if err != nil {
+
+		}
+
+		prog := p.Parse()
+
+		comp := compiler.New()
+
+		err = comp.Compile(prog)
+		if err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New()
+
+		err = vm.Run(comp.Bytecode())
+		if err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		stackElem := vm.StackTop()
+
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testintobj: %s", err)
+		}
+	}
+}
